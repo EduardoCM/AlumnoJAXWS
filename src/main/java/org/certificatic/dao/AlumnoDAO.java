14823ef822015be97bd53d66424de35a08ffb0ea
Eduardo Castillo <eduardocastillo@Eduardos-MacBook-Pro.local>
2018-10-13 14:19:13 -0500

    first commit

---
+++ src/main/java/org/certificatic/dao/AlumnoDAO.java
@@ -0,0 +1,15 @@
+package org.certificatic.dao;
+
+import java.util.List;
+
+import org.certificatic.entity.Alumno;
+
+public interface AlumnoDAO {
+	
+    Alumno guardarAlumno(Alumno alumno);
+	
+	List<Alumno> obtenerAlumnos();
+	
+	
+
+}
